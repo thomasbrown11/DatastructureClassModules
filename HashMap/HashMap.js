f43b8f29445f2af7dd7f6a361cd7c64e8bd58dae
thomasbrown11 <93149238+thomasbrown11@users.noreply.github.com>
2023-02-01 13:57:18 -0500

    initiated several hashmap-related files

---
+++ HashMap/HashMap.js
@@ -0,0 +1,5 @@
+class HashMap {
+    constructor(size = 0) {
+        this.array = new Array(size).fill(null);
+    }
+}
---
+++ HashMap/HashMap.ts
@@ -0,0 +1,8 @@
+
+class HashMap {
+  array: null[] | any[];
+  constructor(size: number = 0) {
+    this.array = new Array(size).fill(null);
+  }
+
+}
