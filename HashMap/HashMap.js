ccd4b89f0f76c3deeb63e65a028829fa1ab9c24a
thomasbrown11 <93149238+thomasbrown11@users.noreply.github.com>
2023-02-01 14:48:05 -0500

    updated HashMap.ts

--- HashMap/HashMap.js
+++ HashMap/HashMap.js
@@ -1,5 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 class HashMap {
     constructor(size = 0) {
-        this.array = new Array(size).fill(null);
+        this.hashmap = new Array(size).fill(null);
+    }
+    hash(key) {
+        let hashCode = 0;
+        for (let i = 0; i < key.length; i++) {
+            hashCode += (key.charCodeAt(i) + hashCode);
+        }
+        return hashCode % this.hashmap.length;
+    }
+    assign(key, value) {
+        const arrayIndex = this.hash(key);
+        this.hashmap[arrayIndex] = value;
+    }
+    retrieve(key) {
+        const arrayIndex = this.hash(key);
+        return this.hashmap[arrayIndex];
     }
 }
+exports.default = HashMap;
--- HashMap/HashMap.ts
+++ HashMap/HashMap.ts
@@ -1,8 +1,29 @@
 
 class HashMap {
-  array: null[] | any[];
+  hashmap: null[] | any[];
+
   constructor(size: number = 0) {
-    this.array = new Array(size).fill(null);
+    this.hashmap = new Array(size).fill(null);
+  }
+
+  public hash(key: string): number {
+    let hashCode: number = 0;
+    for (let i: number = 0; i < key.length; i++) {
+      hashCode += (key.charCodeAt(i) + hashCode);
+    }
+    return hashCode % this.hashmap.length;
+  }
+
+  public assign(key: string, value: any): void {
+    const arrayIndex: number = this.hash(key);
+    this.hashmap[arrayIndex] = value;
+  }
+
+  public retrieve(key: string): any {
+    const arrayIndex: number = this.hash(key);
+    return this.hashmap[arrayIndex];
   }
 
 }
+
+export default HashMap;
