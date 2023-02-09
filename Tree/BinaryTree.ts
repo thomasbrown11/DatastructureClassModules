98671c3e1b4b9555ea3737364552825d5ed627df
thomasbrown11 <93149238+thomasbrown11@users.noreply.github.com>
2023-02-09 15:01:57 -0500

    added insert method to bin tree module

--- Tree/BinaryTree.ts
+++ Tree/BinaryTree.ts
@@ -10,9 +10,32 @@ class BinaryTree {
     this.left = null;
     this.right = null;
   }
+
+  //insert at right or left based on comparison to this.data, call recursively to traverse down to nearest empty node
+  //will do nothing if value matches current value in binary tree
+  public insert(data: number): void {
+    if (data < this.data) {
+      if (this.left) {
+        this.left.insert(data);
+      } else {
+        this.left = new BinaryTree(data, (this.depth + 1));
+      }
+    } else {
+      if (this.right) {
+        this.right.insert(data);
+      } else {
+        this.right = new BinaryTree(data, (this.depth + 1));
+      }
+    }
+  }
 }
 
 const bt = new BinaryTree(15);
+console.log(bt)
+
+bt.insert(3);
+bt.insert(20);
+bt.insert(5);
 console.log(bt)
 
 export default BinaryTree;
